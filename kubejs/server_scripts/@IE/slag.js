a47149906918020d1f6e957e2671ba082adf6bef
FooterMan15 <blakebird12@gmail.com>
2023-12-10 23:01:24 -0500

    Revert "IE Base"
    This reverts commit c8018ee50e917f312518aee5e01c6aadba09fb58.

--- kubejs/server_scripts/@IE/slag.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(r =>{
-    r.remove({input: 'thermal:slag', type: 'smelting'})
-    r.remove({input: 'thermal:slag', type: 'blasting'})
-
-    new doubleSmelt(r, 'thermal:slag', 'kubejs:slag_brick')
-})
