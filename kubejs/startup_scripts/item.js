d7637edb859d4dca7e7a35fcf8588a14a21ef073
FooterMan15 <blakebird12@gmail.com>
2023-12-11 09:28:31 -0500

    Items (Test commit)

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -2,5 +2,9 @@ StartupEvents.registry('item', i =>{
     i.create('all_of_factory')
 
     i.create('porcelain_clay')
-    i.create('emendatusenigmatica:wooden_gear')
+    i.create('leather_bound_stick')
+    i.create('smth', '')
+})
+StartupEvents.registry('item', event =>{
+    event.create('cool_paxel', 'paxel')
 })
